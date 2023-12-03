30c5a764ef4005eddb0eabc02ca1a03aeb694931
Arttu Kosonen <arttu.kosonen@solita.fi>
2023-12-03 21:14:56 +0200

    Day 3 part 2 solved finally, too

--- src/day_3.py
+++ src/day_3.py
@@ -10,7 +10,7 @@ def solve_1(filepath: str) -> int:
 
     number_matches = [
         (int(m.group(0)), m.start(0), m.end(0) - 1)
-        for m in re.finditer("\d+", flat_schematic)
+        for m in re.finditer(r"\d+", flat_schematic)
     ]
     # (number, startpos, endpos)
     # Look behind: startpos-1 and range startpos-rowlen-1 to endpos-rowlen+1
@@ -31,8 +31,47 @@ def solve_1(filepath: str) -> int:
     return sum(numbers_with_neighbors)
 
 
+def solve_2(filepath: str) -> int:
+    schematic = read_file_as_list(filepath)
+    rowlen = len(schematic[0])
+    flat_schematic = "".join(schematic)
+    totallen = len(flat_schematic)
+
+    number_matches = [
+        (int(m.group(0)), m.start(0), m.end(0))
+        for m in re.finditer(r"\d+", flat_schematic)
+    ]
+    number_positions = [(m[0], set((*range(m[1], m[2]),))) for m in number_matches]
+
+    # For *, startpos == endpos
+    star_positions = [m.start(0) for m in re.finditer(r"\*", flat_schematic)]
+    star_boxes = []
+    for p in star_positions:
+        box = (
+            *range(p - rowlen - 1, p - rowlen + 2),
+            *range(p - 1, p + 2),
+            *range(p + rowlen - 1, p + rowlen + 2),
+        )
+        box = set(el for el in box if totallen > el >= 0)
+        star_boxes.append(box)
+
+    gears = []
+    for b in star_boxes:
+        matching_numbers = []
+        for n in number_positions:
+            if b.intersection(n[1]):
+                matching_numbers.append(n[0])
+        gears.append(matching_numbers)
+    gears = [g for g in gears if len(g) == 2]
+
+    gear_ratios = [g[0] * g[1] for g in gears]
+
+    return sum(gear_ratios)
+
+
 def main():
     print(solve_1("../inputs/day_3.txt"))
+    print(solve_2("../inputs/day_3.txt"))
 
 
 if __name__ == "__main__":
--- src/test_day_3.py
+++ src/test_day_3.py
@@ -3,3 +3,7 @@ from day_3 import *
 
 def test_solve_1():
     assert solve_1("../inputs/day_3_example.txt") == 4361
+
+
+def test_solve_2():
+    assert solve_2("../inputs/day_3_example.txt") == 467835
